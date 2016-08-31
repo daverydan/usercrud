31095991f6ed8953dec261dabe2f3f0ad033c3ac
Danny Avery <danny.w.avery@gmail.com>
2016-08-31 09:07:45 -0600

    Add Heroku creds and update default route to hit the DB.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    // return view('welcome');
-    return view('users.index');
+Route::get('/', function()
+{
+    return User::all();
 });
 
-Route::resource('users', 'UsersController');
+// Route::get('/', function () {
+    // return view('welcome');
+//     return view('users.index');
+// });
+
+// Route::resource('users', 'UsersController');
